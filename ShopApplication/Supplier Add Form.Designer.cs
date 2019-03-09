e87dfdaa442e84bd0b2f96056e1bd225e8b07704
mohammed-elkrnshawy <mohammedelkrnshawy@gmail.com>
2019-03-09 17:57:16 +0200

    Add Supplier

--- ShopApplication/Supplier Add Form.Designer.cs
+++ ShopApplication/Supplier Add Form.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.panel7 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel8 = new System.Windows.Forms.Panel();
@@ -37,43 +36,30 @@
             this.bt_save = new System.Windows.Forms.Button();
             this.panel4 = new System.Windows.Forms.Panel();
             this.panel6 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.panel5 = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.txxMoney = new System.Windows.Forms.TextBox();
+            this.txt_Money = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
-            this.txtPhone = new System.Windows.Forms.TextBox();
-            this.txtPhone2 = new System.Windows.Forms.TextBox();
-            this.txAddress = new System.Windows.Forms.TextBox();
-            this.txtCompany = new System.Windows.Forms.TextBox();
-            this.txtName = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
+            this.txt_Phone = new System.Windows.Forms.TextBox();
+            this.txt_Address = new System.Windows.Forms.TextBox();
+            this.txt_Name = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.panel7.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel8.SuspendLayout();
             this.panel4.SuspendLayout();
             this.panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.panel5.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel3.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Location = new System.Drawing.Point(518, 38);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(22, 20);
-            this.pictureBox2.TabIndex = 21;
-            this.pictureBox2.TabStop = false;
             // 
             // panel7
             // 
@@ -83,7 +69,7 @@
             this.panel7.Location = new System.Drawing.Point(0, 0);
             this.panel7.Name = "panel7";
             this.panel7.Size = new System.Drawing.Size(1386, 680);
-            this.panel7.TabIndex = 1;
+            this.panel7.TabIndex = 2;
             // 
             // panel2
             // 
@@ -122,6 +108,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "الغاء";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // bt_edit
             // 
@@ -135,6 +122,7 @@
             this.bt_edit.TabIndex = 1;
             this.bt_edit.Text = "تعديل";
             this.bt_edit.UseVisualStyleBackColor = false;
+            this.bt_edit.Click += new System.EventHandler(this.bt_edit_Click);
             // 
             // bt_save
             // 
@@ -148,7 +136,7 @@
             this.bt_save.TabIndex = 0;
             this.bt_save.Text = "حفظ";
             this.bt_save.UseVisualStyleBackColor = false;
-            this.bt_save.Click += new System.EventHandler(this.bt_save_Click);
+            this.bt_save.Click += new System.EventHandler(this.bt_save_Click_1);
             // 
             // panel4
             // 
@@ -172,12 +160,21 @@
             this.panel6.Size = new System.Drawing.Size(650, 81);
             this.panel6.TabIndex = 21;
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(518, 38);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(22, 20);
+            this.pictureBox2.TabIndex = 21;
+            this.pictureBox2.TabStop = false;
+            // 
             // textBox6
             // 
             this.textBox6.Location = new System.Drawing.Point(108, 38);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(404, 20);
             this.textBox6.TabIndex = 20;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
             // 
             // panel5
             // 
@@ -200,135 +197,98 @@
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(647, 470);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // panel3
             // 
             this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel3.Controls.Add(this.txxMoney);
+            this.panel3.Controls.Add(this.txt_Money);
             this.panel3.Controls.Add(this.label8);
-            this.panel3.Controls.Add(this.txtPhone);
-            this.panel3.Controls.Add(this.txtPhone2);
-            this.panel3.Controls.Add(this.txAddress);
-            this.panel3.Controls.Add(this.txtCompany);
-            this.panel3.Controls.Add(this.txtName);
-            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.txt_Phone);
+            this.panel3.Controls.Add(this.txt_Address);
+            this.panel3.Controls.Add(this.txt_Name);
             this.panel3.Controls.Add(this.label5);
             this.panel3.Controls.Add(this.label4);
-            this.panel3.Controls.Add(this.label3);
             this.panel3.Controls.Add(this.label2);
             this.panel3.Location = new System.Drawing.Point(0, 9);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(650, 560);
             this.panel3.TabIndex = 0;
             // 
-            // txxMoney
+            // txt_Money
             // 
-            this.txxMoney.Location = new System.Drawing.Point(123, 422);
-            this.txxMoney.Name = "txxMoney";
-            this.txxMoney.Size = new System.Drawing.Size(196, 20);
-            this.txxMoney.TabIndex = 29;
-            this.txxMoney.Text = "0";
-            this.txxMoney.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_Money.Location = new System.Drawing.Point(123, 346);
+            this.txt_Money.Name = "txt_Money";
+            this.txt_Money.Size = new System.Drawing.Size(196, 20);
+            this.txt_Money.TabIndex = 37;
+            this.txt_Money.Text = "0";
+            this.txt_Money.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_Money.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Money_KeyPress);
             // 
             // label8
             // 
             this.label8.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
-            this.label8.Location = new System.Drawing.Point(371, 414);
+            this.label8.Location = new System.Drawing.Point(371, 338);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(155, 30);
-            this.label8.TabIndex = 28;
+            this.label8.TabIndex = 36;
             this.label8.Text = "اجمالى المبلغ";
             this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // txtPhone
+            // txt_Phone
             // 
-            this.txtPhone.Location = new System.Drawing.Point(123, 318);
-            this.txtPhone.Name = "txtPhone";
-            this.txtPhone.Size = new System.Drawing.Size(196, 20);
-            this.txtPhone.TabIndex = 20;
-            this.txtPhone.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_Phone.Location = new System.Drawing.Point(123, 299);
+            this.txt_Phone.Name = "txt_Phone";
+            this.txt_Phone.Size = new System.Drawing.Size(196, 20);
+            this.txt_Phone.TabIndex = 35;
+            this.txt_Phone.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_Phone.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Phone_KeyPress);
             // 
-            // txtPhone2
+            // txt_Address
             // 
-            this.txtPhone2.Location = new System.Drawing.Point(123, 371);
-            this.txtPhone2.Name = "txtPhone2";
-            this.txtPhone2.Size = new System.Drawing.Size(196, 20);
-            this.txtPhone2.TabIndex = 19;
-            this.txtPhone2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_Address.Location = new System.Drawing.Point(123, 249);
+            this.txt_Address.Name = "txt_Address";
+            this.txt_Address.Size = new System.Drawing.Size(196, 20);
+            this.txt_Address.TabIndex = 34;
+            this.txt_Address.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // txAddress
+            // txt_Name
             // 
-            this.txAddress.Location = new System.Drawing.Point(123, 268);
-            this.txAddress.Name = "txAddress";
-            this.txAddress.Size = new System.Drawing.Size(196, 20);
-            this.txAddress.TabIndex = 18;
-            this.txAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // txtCompany
-            // 
-            this.txtCompany.Location = new System.Drawing.Point(123, 216);
-            this.txtCompany.Name = "txtCompany";
-            this.txtCompany.Size = new System.Drawing.Size(196, 20);
-            this.txtCompany.TabIndex = 17;
-            this.txtCompany.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // txtName
-            // 
-            this.txtName.Location = new System.Drawing.Point(123, 174);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(196, 20);
-            this.txtName.TabIndex = 16;
-            this.txtName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // label6
-            // 
-            this.label6.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
-            this.label6.Location = new System.Drawing.Point(371, 363);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(155, 30);
-            this.label6.TabIndex = 15;
-            this.label6.Text = "رقم التليفون";
-            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.txt_Name.Location = new System.Drawing.Point(123, 199);
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(196, 20);
+            this.txt_Name.TabIndex = 33;
+            this.txt_Name.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label5
             // 
             this.label5.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
-            this.label5.Location = new System.Drawing.Point(371, 310);
+            this.label5.Location = new System.Drawing.Point(371, 291);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(155, 30);
-            this.label5.TabIndex = 14;
+            this.label5.TabIndex = 32;
             this.label5.Text = "رقم الشركة";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label4
             // 
             this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
-            this.label4.Location = new System.Drawing.Point(371, 260);
+            this.label4.Location = new System.Drawing.Point(371, 241);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(155, 30);
-            this.label4.TabIndex = 13;
+            this.label4.TabIndex = 31;
             this.label4.Text = "عنوان الشركة";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label3
-            // 
-            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(371, 208);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(155, 30);
-            this.label3.TabIndex = 12;
-            this.label3.Text = "اسم الشركة";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(371, 166);
+            this.label2.Location = new System.Drawing.Point(371, 191);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(155, 30);
-            this.label2.TabIndex = 11;
+            this.label2.TabIndex = 30;
             this.label2.Text = "اسم المورد";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -342,45 +302,36 @@
             this.label1.Text = "اصافة و تعديل المورد";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.panel7);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1386, 680);
-            this.panel1.TabIndex = 2;
-            // 
             // Supplier_Add_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1386, 680);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel7);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Supplier_Add_Form";
             this.Text = "Supplier_Add_Form";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.Load += new System.EventHandler(this.Supplier_Add_Form_Load);
             this.panel7.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel8.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             this.panel6.ResumeLayout(false);
             this.panel6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.panel5.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
-            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel8;
         private System.Windows.Forms.Button button3;
@@ -388,23 +339,18 @@
         private System.Windows.Forms.Button bt_save;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.TextBox txxMoney;
+        private System.Windows.Forms.TextBox txt_Money;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox txtPhone;
-        private System.Windows.Forms.TextBox txtPhone2;
-        private System.Windows.Forms.TextBox txAddress;
-        private System.Windows.Forms.TextBox txtCompany;
-        private System.Windows.Forms.TextBox txtName;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_Phone;
+        private System.Windows.Forms.TextBox txt_Address;
+        private System.Windows.Forms.TextBox txt_Name;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel panel1;
     }
 }
--- ShopApplication/Supplier Add Form.cs
+++ ShopApplication/Supplier Add Form.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,14 +13,156 @@ namespace ShopApplication
 {
     public partial class Supplier_Add_Form : Form
     {
+        private DataSet ds;
+        private int Customer_ID;
+
         public Supplier_Add_Form()
         {
             InitializeComponent();
         }
 
-        private void bt_save_Click(object sender, EventArgs e)
+        private void Supplier_Add_Form_Load(object sender, EventArgs e)
         {
+            RefForm();
+            //dataGridView1.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
 
+        private void RefForm()
+        {
+            txt_Money.Enabled = true;
+            bt_save.Enabled = true;
+            bt_edit.Enabled = false;
+
+
+            txt_Name.Text=txt_Phone.Text=txt_Address.Text = "";
+            txt_Money.Text = "0";
+
+            using (ds = Ezzat.GetDataSet("Supplier_selectAll", "X"))
+            {
+                dataGridView1.DataSource = ds.Tables["X"];
+            }
+        }
+
+        private void bt_save_Click_1(object sender, EventArgs e)
+        {
+            ValidDataSave();
+        }
+
+        private void ValidDataSave()
+        {
+            if (ValidText(txt_Name) && ValidText(txt_Address) && ValidText(txt_Money) && ValidText(txt_Phone))
+            {
+                SaveData();
+                RefForm();
+            }
+            else
+            {
+                MessageBox.Show(SharedClass.Check_Message);
+            }
+        }
+
+        private void SaveData()
+        {
+            Ezzat.ExecutedNoneQuery("insert_Supplier",
+                    new SqlParameter("@Supplier_Name", txt_Name.Text),
+                    new SqlParameter("@Supplier_Address", txt_Address.Text),
+                    new SqlParameter("@Supplier_Phone", txt_Phone.Text),
+                    new SqlParameter("@total", double.Parse(txt_Money.Text))
+                    );
+
+            MessageBox.Show(SharedClass.Successful_Message);
+        }
+
+        private bool ValidText(TextBox text)
+        {
+            if (text.Text == "")
+                return false;
+            else
+                return true;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txt_Money.Enabled = false;
+            Customer_ID = (int)dataGridView1.CurrentRow.Cells[0].Value;
+            ShowDetails(Customer_ID);
+        }
+
+        private void ShowDetails(int customer_ID)
+        {
+            bt_edit.Enabled = true;
+            bt_save.Enabled = false;
+
+            SqlConnection con;
+            SqlDataReader dataReader = Ezzat.GetDataReader("Supplier_selectSearch_BYID", out con, new SqlParameter("@Customer_Id", customer_ID));
+
+
+            if (dataReader.HasRows)
+            {
+                while (dataReader.Read())
+                {
+                    txt_Name.Text = dataReader["Supplier_Name"].ToString();
+                    txt_Address.Text = dataReader["Supplier_Address"].ToString();
+                    txt_Phone.Text = dataReader["Supplier_Phone"].ToString();
+                    txt_Money.Text = dataReader["Supplier_Money"].ToString();
+
+                }
+            }
+            con.Close();
+
+
+        }
+
+        private void bt_edit_Click(object sender, EventArgs e)
+        {
+            ValidDataEdit();
+        }
+
+        private void ValidDataEdit()
+        {
+            if (ValidText(txt_Name) && ValidText(txt_Address) && ValidText(txt_Phone))
+            {
+                EditData();
+                RefForm();
+            }
+            else
+            {
+                MessageBox.Show(SharedClass.Check_Message);
+            }
+        }
+
+        private void EditData()
+        {
+            Ezzat.ExecutedNoneQuery("Supplier_updateSupplier"
+                , new SqlParameter("@Supplier_Name", txt_Name.Text)
+                , new SqlParameter("@Supplier_Address", txt_Address.Text)
+                , new SqlParameter("@Supplier_Phone", txt_Phone.Text)
+                , new SqlParameter("@Customer_ID", Customer_ID)
+                );
+            MessageBox.Show(SharedClass.Edit_Message);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            RefForm();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            using (ds = Ezzat.GetDataSet("Supplier_selectSearch", "X", new SqlParameter("text", textBox6.Text)))
+            {
+                dataGridView1.DataSource = ds.Tables["X"];
+            }
+        }
+
+        private void txt_Phone_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            SharedClass.KeyPress(txt_Phone, e);
+        }
+
+        private void txt_Money_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            SharedClass.KeyPress(txt_Money, e);
         }
     }
 }
